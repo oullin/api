0408d1bfef5de77e67a683dd981de565dee72672
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-19 16:40:33 +0800

    format

--- cli/gate/guard.go
+++ cli/gate/guard.go
@@ -1,40 +1,40 @@
 package gate
 
 import (
-    "bufio"
-    "fmt"
-    "github.com/oullin/pkg/auth"
-    "github.com/oullin/pkg/cli"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"github.com/oullin/pkg/auth"
+	"github.com/oullin/pkg/cli"
+	"os"
+	"strings"
 )
 
 type Guard struct {
-    salt   string
-    token  auth.Token
-    reader *bufio.Reader
+	salt   string
+	token  auth.Token
+	reader *bufio.Reader
 }
 
 func MakeGuard(token auth.Token) Guard {
-    return Guard{
-        token:  token,
-        reader: bufio.NewReader(os.Stdin),
-    }
+	return Guard{
+		token:  token,
+		reader: bufio.NewReader(os.Stdin),
+	}
 }
 
 func (g *Guard) CaptureInput() error {
-    cli.Warning("Type the public token: ")
-    input, err := g.reader.ReadString('\n')
+	cli.Warning("Type the public token: ")
+	input, err := g.reader.ReadString('\n')
 
-    if err != nil {
-        return fmt.Errorf("%s error reading input: %v %s", cli.RedColour, err, cli.Reset)
-    }
+	if err != nil {
+		return fmt.Errorf("%s error reading input: %v %s", cli.RedColour, err, cli.Reset)
+	}
 
-    g.salt = strings.TrimSpace(input)
+	g.salt = strings.TrimSpace(input)
 
-    return nil
+	return nil
 }
 
 func (g *Guard) Rejects() bool {
-    return g.token.IsInvalid(g.salt)
+	return g.token.IsInvalid(g.salt)
 }
