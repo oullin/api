74124a8f8197e8563dda944aa8777d6392a60a2d
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-23 10:52:04 +0800

    format

--- pkg/http/middleware/pipeline.go
+++ pkg/http/middleware/pipeline.go
@@ -1,21 +1,21 @@
 package middleware
 
 import (
-    "github.com/oullin/env"
-    "github.com/oullin/pkg/http"
+	"github.com/oullin/env"
+	"github.com/oullin/pkg/http"
 )
 
 type Pipeline struct {
-    Env *env.Environment
+	Env *env.Environment
 }
 
 // Chain applies a list of middleware handlers to a final ApiHandler.
 // It builds the chain in reverse, so the first middleware
 // in the list is the outermost one, executing first.
 func (m Pipeline) Chain(h http.ApiHandler, handlers ...http.Middleware) http.ApiHandler {
-    for i := len(handlers) - 1; i >= 0; i-- {
-        h = handlers[i](h)
-    }
+	for i := len(handlers) - 1; i >= 0; i-- {
+		h = handlers[i](h)
+	}
 
-    return h
+	return h
 }
