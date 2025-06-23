301d0eba17bfc5baeeeaf9f383e459c638ad1551
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-23 15:26:16 +0800

    Fix logging level for error messages.

--- pkg/http/middleware/username.go
+++ pkg/http/middleware/username.go
@@ -1,31 +1,31 @@
 package middleware
 
 import (
-    "fmt"
-    "github.com/oullin/pkg/http"
-    "log/slog"
-    baseHttp "net/http"
-    "strings"
+	"fmt"
+	"github.com/oullin/pkg/http"
+	"log/slog"
+	baseHttp "net/http"
+	"strings"
 )
 
 const usernameHeader = "X-API-Username"
 
 func UsernameCheck(next http.ApiHandler) http.ApiHandler {
-    return func(w baseHttp.ResponseWriter, r *baseHttp.Request) *http.ApiError {
-        username := strings.TrimSpace(r.Header.Get(usernameHeader))
+	return func(w baseHttp.ResponseWriter, r *baseHttp.Request) *http.ApiError {
+		username := strings.TrimSpace(r.Header.Get(usernameHeader))
 
-        if username != "gocanto" {
-            message := fmt.Sprintf("Unauthorized: Invalid API username received ('%s')", username)
-            slog.Error(message)
+		if username != "gocanto" {
+			message := fmt.Sprintf("Unauthorized: Invalid API username received ('%s')", username)
+			slog.Error(message)
 
-            return &http.ApiError{
-                Message: message,
-                Status:  baseHttp.StatusUnauthorized,
-            }
-        }
+			return &http.ApiError{
+				Message: message,
+				Status:  baseHttp.StatusUnauthorized,
+			}
+		}
 
-        slog.Info("Successfully authenticated user: gocanto")
+		slog.Info("Successfully authenticated user: gocanto")
 
-        return next(w, r)
-    }
+		return next(w, r)
+	}
 }
