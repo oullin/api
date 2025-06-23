093c810da395c029f639c0537a5cb075e311392d
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-23 15:32:58 +0800

    format

--- boost/helpers.go
+++ boost/helpers.go
@@ -1,45 +1,45 @@
 package boost
 
 import (
-    "github.com/oullin/database"
-    "github.com/oullin/env"
-    baseHttp "net/http"
+	"github.com/oullin/database"
+	"github.com/oullin/env"
+	baseHttp "net/http"
 )
 
 func (a *App) SetRouter(router Router) {
-    a.router = &router
+	a.router = &router
 }
 
 func (a *App) CloseLogs() {
-    if a.logs == nil {
-        return
-    }
+	if a.logs == nil {
+		return
+	}
 
-    driver := *a.logs
-    driver.Close()
+	driver := *a.logs
+	driver.Close()
 }
 
 func (a *App) CloseDB() {
-    if a.db == nil {
-        return
-    }
+	if a.db == nil {
+		return
+	}
 
-    driver := *a.db
-    driver.Close()
+	driver := *a.db
+	driver.Close()
 }
 
 func (a *App) GetEnv() *env.Environment {
-    return a.env
+	return a.env
 }
 
 func (a *App) GetDB() *database.Connection {
-    return a.db
+	return a.db
 }
 
 func (a *App) GetMux() *baseHttp.ServeMux {
-    if a.router == nil {
-        return nil
-    }
+	if a.router == nil {
+		return nil
+	}
 
-    return a.router.Mux
+	return a.router.Mux
 }
