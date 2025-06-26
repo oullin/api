b11949977b328a94dc77ec8ac9937d2cd83addf3
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-26 16:47:30 +0800

    add build command

--- config/makefile/build.mk
+++ config/makefile/build.mk
@@ -7,5 +7,10 @@ build\:prod:
 	docker compose --profile prod up --build -d
 
 build\:release:
-	git tag v$(V)
-	@read -p "Press enter to confirm and push to origin ..." && git push origin v$(V)
+	@printf "\n$(YELLOW)Tagging images to be released.$(NC)\n"
+	docker tag api-api ghcr.io/gocanto/oullin_api:0.0.1 && \
+	docker tag api-caddy_prod ghcr.io/gocanto/oullin_proxy:0.0.1
+
+	@printf "\n$(CYAN)Pushing release to GitHub registry.$(NC)\n"
+	docker push ghcr.io/gocanto/oullin_api:0.0.1 && \
+	docker push ghcr.io/gocanto/oullin_proxy:0.0.1
