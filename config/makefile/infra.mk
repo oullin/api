4d469bf0d7f9a00cd9ddbf344411b71d1b653c57
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-06-27 16:53:26 +0800

    format

--- config/makefile/infra.mk
+++ config/makefile/infra.mk
@@ -4,25 +4,19 @@
 ___API__SUPERVISOR := oullin-api
 
 supv\:api\:status:
-	@printf "\n$(YELLOW)[supervisor]$(NC) - $(CYAN)$(___API__SUPERVISOR) status.$(NC)"
 	@sudo supervisorctl status $(___API__SUPERVISOR)
 
 supv\:api\:start:
-	@printf "\n$(YELLOW)[supervisor]$(NC) - $(CYAN)$(___API__SUPERVISOR) start command sent.$(NC)"
 	@sudo supervisorctl start $(___API__SUPERVISOR)
 
 supv\:api\:stop:
-	@printf "\n$(YELLOW)[supervisor]$(NC) - $(CYAN)$(___API__SUPERVISOR) stop command sent.$(NC)"
 	@sudo supervisorctl stop $(___API__SUPERVISOR)
 
 supv\:api\:restart:
-	@printf "\n$(YELLOW)[supervisor]$(NC) - $(CYAN)$(___API__SUPERVISOR) restart command sent.$(NC)"
 	@sudo supervisorctl restart $(___API__SUPERVISOR)
 
 supv\:api\:logs:
-	@printf "\n$(YELLOW)[supervisor]$(NC) - $(CYAN)$(___API__SUPERVISOR) logs. (Press Ctrl+C to exit)$(NC)"
 	@sudo tail -f /var/log/supervisor/$(___API__SUPERVISOR).log
 
 supv\:api\:logs-err:
-	@printf "\n$(YELLOW)[supervisor]$(NC) - $(CYAN)$(___API__SUPERVISOR) error logs. (Press Ctrl+C to exit)$(NC)"
 	@sudo tail -f /var/log/supervisor/$(___API__SUPERVISOR).err.log
