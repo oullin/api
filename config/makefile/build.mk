d6ad37c14ec6458af94617278443489fb6fd1e5f
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-07 16:40:14 +0800

    wip

--- config/makefile/build.mk
+++ config/makefile/build.mk
@@ -1,31 +1,31 @@
-.PHONY: build\:local build\:prod build\:release build\:deploy
+.PHONY: build-local build-prod build-release build-deploy
 
 BUILD_VERSION ?= latest
 BUILD_PACKAGE_OWNER := oullin
 
-build\:local:
+build-local:
 	docker compose --profile local up --build -d
 
-build\:ci:
+build-ci:
 	@printf "\n$(CYAN)Building production images for CI$(NC)\n"
 	# This 'build' command only builds the images; it does not run them.
 	@docker compose --profile prod build
 
 # --- Deprecated
 #     We should always deploy builds from the CI and not build again in servers.
-build\:prod:
+build-prod:
 	@DB_SECRET_USERNAME="$(DB_SECRET_USERNAME)" \
 	DB_SECRET_PASSWORD="$(DB_SECRET_PASSWORD)" \
 	DB_SECRET_DBNAME="$(DB_SECRET_DBNAME)" \
 	docker compose --profile prod up --build -d
 
-build\:deploy:
+build-deploy:
 	@DB_SECRET_USERNAME="$(DB_SECRET_USERNAME)" \
 	DB_SECRET_PASSWORD="$(DB_SECRET_PASSWORD)" \
 	DB_SECRET_DBNAME="$(DB_SECRET_DBNAME)" \
 	docker compose --profile prod up -d
 
-build\:release:
+build-release:
 	@printf "\n$(YELLOW)Tagging images to be released.$(NC)\n"
 	docker tag api-api ghcr.io/$(BUILD_PACKAGE_OWNER)/oullin_api:$(BUILD_VERSION) && \
 	docker tag api-caddy_prod ghcr.io/$(BUILD_PACKAGE_OWNER)/oullin_proxy:$(BUILD_VERSION)
