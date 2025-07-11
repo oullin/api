c487d231fa8365d8945bb7c76eb0df2d49eb2796
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-11 11:01:48 +0800

    migrations

--- database/infra/scripts/run-migration.sh
+++ database/infra/scripts/run-migration.sh
@@ -4,9 +4,9 @@
 set -e
 
 # Read the credentials from the Docker Secret files
-DB_USER=$(cat /run/secrets/postgres_user)
-DB_PASSWORD=$(cat /run/secrets/postgres_password)
-DB_NAME=$(cat /run/secrets/postgres_db)
+DB_USER=$(cat /run/secrets/pg_username)
+DB_PASSWORD=$(cat /run/secrets/pg_password)
+DB_NAME=$(cat /run/secrets/pg_dbname)
 
 # Construct the database URL using the values from the secrets
 DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@api-db:5432/${DB_NAME}?sslmode=require"
