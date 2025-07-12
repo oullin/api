04abafa0a7f922162debd9b4e9fd181128d073a0
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-12 14:19:10 +0800

    add db secrets to runner

--- config/makefile/app.mk
+++ config/makefile/app.mk
@@ -30,3 +30,25 @@ install-air:
 	#     https://github.com/air-verse/air
 	@echo "Installing air ..."
 	@go install github.com/air-verse/air@latest
+
+YEL := \033[0;33m
+GRN := \033[0;32m
+NC  := \033[0m
+
+app-cli:
+	@if [ -z "$(DB_SECRET_USERNAME)" ] || [ -z "$(DB_SECRET_PASSWORD)" ] || [ -z "$(DB_SECRET_DBNAME)" ]; then \
+    	  printf "\n$(RED)⚠️ Usage: make app-cli \n$(NC)"; \
+    	  printf "         DB_SECRET_USERNAME=/path/to/pg_username\n"; \
+    	  printf "         DB_SECRET_PASSWORD=/path/to/pg_password\n"; \
+    	  printf "         DB_SECRET_DBNAME=/path/to/pg_dbname\n\n"; \
+    	  printf "\n------------------------------------------------------\n\n"; \
+    	  exit 1; \
+    	fi; \
+    	printf "\n$(GREEN)🔒 Running CLI with secrets from:$(NC)\n"; \
+    	printf "           DB_SECRET_USERNAME=$(DB_SECRET_USERNAME)\n"; \
+    	printf "           DB_SECRET_PASSWORD=$(DB_SECRET_PASSWORD)\n"; \
+    	printf "           DB_SECRET_DBNAME=$(DB_SECRET_DBNAME)\n\n"; \
+    	DB_SECRET_USERNAME="$(DB_SECRET_USERNAME)" \
+    	DB_SECRET_PASSWORD="$(DB_SECRET_PASSWORD)" \
+    	DB_SECRET_DBNAME="$(DB_SECRET_DBNAME)" \
+    	docker compose run --rm api-runner go run $(ROOT_PATH)/cli/main.go
