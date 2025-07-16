9f626a689de898a36a466064d554788afba5d685
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-07-16 10:33:35 +0800

    start workin on Bearer-Token

---
+++ database/infra/migrations/000002_api_keys.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE api_keys (
+	id BIGSERIAL PRIMARY KEY,
+	uuid UUID UNIQUE NOT NULL,
+	account_name VARCHAR(50) UNIQUE NOT NULL,
+	public_key VARCHAR(50) UNIQUE NOT NULL,
+	secret_key VARCHAR(50) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	deleted_at TIMESTAMP DEFAULT NULL,
+
+	CONSTRAINT uq_account_keys UNIQUE (account_name, public_key, secret_key)
+);
+
+CREATE INDEX idx_account_name ON api_keys(account_name);
+CREATE INDEX idx_public_key ON api_keys(public_key);
+CREATE INDEX idx_secret_key ON api_keys(secret_key);
+CREATE INDEX idx_deleted_at ON api_keys(deleted_at);
