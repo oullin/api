bcbe80f66d0cd752c3573f9a9da224122fde0f8d
Gus <gocanto@users.noreply.github.com>
2025-08-01 14:30:17 +0800

    test: expand isValidTable edge cases

--- database/model_internal_test.go
+++ database/model_internal_test.go
@@ -1,6 +1,9 @@
 package database
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestIsValidTable(t *testing.T) {
 	if !isValidTable("users") {
@@ -8,5 +11,24 @@ func TestIsValidTable(t *testing.T) {
 	}
 	if isValidTable("unknown") {
 		t.Fatalf("unexpected valid table")
+	}
+}
+
+func TestIsValidTableEdgeCases(t *testing.T) {
+	invalid := []string{
+		"",
+		"user!@#",
+		"user-name",
+		"Users",
+		"USERS",
+		"table123",
+		strings.Repeat("x", 256),
+		"   ",
+	}
+
+	for _, name := range invalid {
+		if isValidTable(name) {
+			t.Fatalf("%q should be invalid", name)
+		}
 	}
 }
