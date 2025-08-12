912884ce490d3a5b77a53aaf631967314426c973
Gus <gocanto@users.noreply.github.com>
2025-08-12 13:55:59 +0800

    test: use octal escape sequences for CLI colours

--- pkg/cli/colour_test.go
+++ pkg/cli/colour_test.go
@@ -8,15 +8,15 @@ func TestColourConstants(t *testing.T) {
 		got  string
 		want string
 	}{
-		{"Reset", Reset, "\x1b[0m"},
-		{"RedColour", RedColour, "\x1b[31m"},
-		{"GreenColour", GreenColour, "\x1b[32m"},
-		{"YellowColour", YellowColour, "\x1b[33m"},
-		{"BlueColour", BlueColour, "\x1b[34m"},
-		{"MagentaColour", MagentaColour, "\x1b[35m"},
-		{"CyanColour", CyanColour, "\x1b[36m"},
-		{"GrayColour", GrayColour, "\x1b[37m"},
-		{"WhiteColour", WhiteColour, "\x1b[97m"},
+		{"Reset", Reset, "\033[0m"},
+		{"RedColour", RedColour, "\033[31m"},
+		{"GreenColour", GreenColour, "\033[32m"},
+		{"YellowColour", YellowColour, "\033[33m"},
+		{"BlueColour", BlueColour, "\033[34m"},
+		{"MagentaColour", MagentaColour, "\033[35m"},
+		{"CyanColour", CyanColour, "\033[36m"},
+		{"GrayColour", GrayColour, "\033[37m"},
+		{"WhiteColour", WhiteColour, "\033[97m"},
 	}
 
 	for _, tt := range tests {
