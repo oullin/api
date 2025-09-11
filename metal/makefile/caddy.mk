2fd4f04c5b11e067e0b68fcca5d0c6fc0a588b37
Gustavo Ocanto <gustavoocanto@gmail.com>
2025-09-11 12:14:00 +0800

    ca.srl

--- metal/makefile/caddy.mk
+++ metal/makefile/caddy.mk
@@ -7,8 +7,10 @@ APP_CADDY_CONFIG_LOCAL_FILE ?= caddy/Caddyfile.local
 caddy-gen-cert:
 	openssl genrsa -out $(CADDY_MTLS_DIR)/ca.key 4096
 	openssl req -x509 -new -nodes -key $(CADDY_MTLS_DIR)/ca.key -sha256 -days 3650 -subj "/CN=oullin-mtls-ca" -out $(CADDY_MTLS_DIR)/ca.pem
+	openssl rand -hex 16 | tr '[:lower:]' '[:upper:]' > $(CADDY_MTLS_DIR)/ca.srl
 	chmod 600 $(CADDY_MTLS_DIR)/ca.key
 	chmod 644 $(CADDY_MTLS_DIR)/ca.pem
+	chmod 644 $(CADDY_MTLS_DIR)/ca.srl
 
 # --- Mac:
 #     Needs to be locally installed: https://formulae.brew.sh/formula/caddy
